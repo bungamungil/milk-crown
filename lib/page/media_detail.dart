6ee1c45f9f4f6a06d49962b4dc1883a5d39f2ddf
Naufal Fachrian <naufal.fachrian@gmail.com>
2018-03-13 23:25:08 +0700

    media detail #1

--- lib/page/media_detail.dart
+++ lib/page/media_detail.dart
@@ -13,9 +13,7 @@ class MediaDetailPage extends StatelessWidget {
     var cover;
     try {
       cover = media['attributes']['coverImage']['original'];
-    } catch(exception) {
-      cover = '';
-    }
+    } catch(_) {}
     return new Scaffold(
       body: new CustomScrollView(
         slivers: <Widget>[
@@ -25,7 +23,7 @@ class MediaDetailPage extends StatelessWidget {
             flexibleSpace: new FlexibleSpaceBar(
               background: new FadeInImage(
                 placeholder: new AssetImage('assets/images/placeholder_media.jpg'),
-                image: media != null
+                image: cover != null
                   ? new NetworkImage(cover)
                   : new AssetImage('assets/images/placeholder_media.jpg'),
                 fit: BoxFit.cover,
@@ -33,6 +31,174 @@ class MediaDetailPage extends StatelessWidget {
                 fadeOutDuration: new Duration(milliseconds: 0),
               ),
             ),
+          ),
+          new SliverList(
+            delegate: new SliverChildBuilderDelegate((context, index) {
+              var poster;
+              try {
+                poster = media['attributes']['posterImage']['medium'];
+              } catch (_) {}
+              var title = '';
+              try {
+                title = media['attributes']['canonicalTitle'];
+              } catch (_) {}
+              var synopsis = '';
+              try {
+                synopsis = media['attributes']['synopsis'];
+              } catch (_) {}
+              var averageRating = '';
+              try {
+                averageRating = media['attributes']['averageRating'];
+              } catch (_) {}
+              var popularityRank = '';
+              try {
+                popularityRank = media['attributes']['popularityRank'];
+              } catch (_) {}
+              var type = '';
+              try {
+                type = media['type'];
+              } catch (_) {}
+              var ratingRank = '';
+              try {
+                ratingRank = media['attributes']['ratingRank'];
+              } catch (_) {}
+              return new Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Flexible(
+                    child: new Padding(
+                      padding: new EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+                      child: new FadeInImage(
+                        placeholder: new AssetImage('assets/images/placeholder_media.jpg'),
+                        image: poster != null ? new NetworkImage(poster) : null,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    flex: 3,
+                  ),
+                  new Expanded(
+                    child: new Padding(
+                      padding: new EdgeInsets.fromLTRB(0.0, 16.0, 16.0, 16.0),
+                      child: new Column(
+                        children: <Widget>[
+                          new Padding(padding: new EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 8.0),
+                            child: new Text(title,
+                              style: new TextStyle(
+                                fontFamily: 'Delius',
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.w700
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                          new Padding(padding: new EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
+                            child: new Row(
+                              children: <Widget>[
+                                new Padding(padding: new EdgeInsets.fromLTRB(4.0, 0.0, 12.0, 0.0),
+                                  child: new IconTheme(
+                                    data: new IconThemeData(
+                                      color: Colors.green,
+                                      size: 24.0,
+                                    ),
+                                    child: new Icon(Icons.people),
+                                  ),
+                                ),
+                                new Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    new Text('$averageRating%',
+                                      style: new TextStyle(
+                                        fontFamily: 'Delius',
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.w700
+                                      ),
+                                    ),
+                                    new Text('Community Approval',
+                                      style: new TextStyle(
+                                        fontFamily: 'Delius',
+                                        fontSize: 12.0
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            )
+                          ),
+                          new Padding(padding: new EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
+                            child: new Row(
+                              children: <Widget>[
+                                new Padding(padding: new EdgeInsets.fromLTRB(4.0, 0.0, 12.0, 0.0),
+                                  child: new IconTheme(
+                                    data: new IconThemeData(
+                                      color: Colors.red,
+                                      size: 24.0,
+                                    ),
+                                    child: new Icon(Icons.favorite),
+                                  ),
+                                ),
+                                new Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    new Text('#$popularityRank',
+                                      style: new TextStyle(
+                                        fontFamily: 'Delius',
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.w700
+                                      ),
+                                    ),
+                                    new Text('Most Popular ${type[0].toUpperCase()}${type.substring(1)}',
+                                      style: new TextStyle(
+                                        fontFamily: 'Delius',
+                                        fontSize: 12.0
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            )
+                          ),
+                          new Padding(padding: new EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
+                            child: new Row(
+                              children: <Widget>[
+                                new Padding(padding: new EdgeInsets.fromLTRB(4.0, 0.0, 12.0, 0.0),
+                                  child: new IconTheme(
+                                    data: new IconThemeData(
+                                      color: Colors.orange,
+                                      size: 24.0
+                                    ),
+                                    child: new Icon(Icons.star),
+                                  ),
+                                ),
+                                new Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    new Text('#$ratingRank',
+                                      style: new TextStyle(
+                                        fontFamily: 'Delius',
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.w700
+                                      ),
+                                    ),
+                                    new Text('Highest Rated ${type[0].toUpperCase()}${type.substring(1)}',
+                                      style: new TextStyle(
+                                        fontFamily: 'Delius',
+                                        fontSize: 12.0
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            )
+                          ),
+                        ],
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                      ),
+                    ),
+                    flex: 4,
+                  ),
+                ],
+              );
+            }, childCount: 1),
           ),
         ],
       ),
